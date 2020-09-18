b15b17f02e819cad680d902cf35dbdff4ef65c8d
andre_risso <andre_risso@hst.com.br>
2020-09-17 21:06:40 -0300

    perdi algumas mensagens de commit pq tentei subir uma imagem docker mas era muito grande

--- src/main/java/com/bank/agencies/endpoint/AgenciesController.java
+++ src/main/java/com/bank/agencies/endpoint/AgenciesController.java
@@ -9,10 +9,10 @@
     import org.springframework.web.bind.annotation.*;
 
     import java.util.List;
-    import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.stream.Collectors;
 
     @RestController
-    @RequestMapping(value = "/agencies", produces = MediaType.APPLICATION_JSON_VALUE)
     public class AgenciesController {
 
         private final FindAllAgenciesUseCase findAllAgenciesUseCase;
@@ -23,7 +23,57 @@
 
         @GetMapping
         @ResponseStatus(HttpStatus.OK)
+        @RequestMapping(value = "/agencies", produces = MediaType.APPLICATION_JSON_VALUE)
         public ResponseEntity<List<AgencyResponse>> findAllAgencies() {
+
+            List<AgencyGatewayResponse> agencies = findAllAgenciesUseCase.execute();
+
+            List<AgencyResponse> agencyResponse = agencies.stream()
+                    .map(agencyGateway -> AgencyResponse.AgencyResponseBuilder.anAgencyResponse()
+                    .bank(agencyGateway.getBank())
+                    .city(agencyGateway.getCity())
+                    .name(agencyGateway.getName())
+                    .state(agencyGateway.getState()).build())
+                    .collect(Collectors.toList());
+
+            return new ResponseEntity<>(agencyResponse, HttpStatus.OK);
+        }
+        
+        @GetMapping
+        @ResponseStatus(HttpStatus.OK)
+        @RequestMapping(value = "/all_grouped_agencies", produces = MediaType.APPLICATION_JSON_VALUE)
+        public ResponseEntity<Map<String, List<AgencyResponse>>> findAllGroupedAgencies() {
+
+            List<AgencyGatewayResponse> agencies = findAllAgenciesUseCase.execute();
+
+            List<AgencyResponse> agencyResponse = agencies.stream()
+                    .map(agencyGateway -> AgencyResponse.AgencyResponseBuilder.anAgencyResponse()
+                    .bank(agencyGateway.getBank())
+                    .city(agencyGateway.getCity())
+                    .name(agencyGateway.getName())
+                    .state(agencyGateway.getState()).build())
+                    .collect(Collectors.toList());
+            
+            // ordenando por cidades
+            agencyResponse.sort((AgencyResponse ar1, AgencyResponse ar2) -> ar1.getCity().compareTo(ar2.getCity()));
+            
+            // agrupando por estado
+            Map<String, List<AgencyResponse>> mapGroupAgencyResponse = agencyResponse.stream()
+            		.collect(Collectors.groupingBy(AgencyResponse::getState));
+
+            return new ResponseEntity<>(mapGroupAgencyResponse, HttpStatus.OK);
+        }
+        
+        
+        
+        /*
+         * solução proposta para ganho de performace seria o uso de paginacao
+         * buscando de um determinado numero de registros por vez. 
+         */
+        @ResponseStatus(HttpStatus.OK)
+        @GetMapping(value = "{page}/{count}")
+        @RequestMapping(value = "/agenciesPages", produces = MediaType.APPLICATION_JSON_VALUE)
+        public ResponseEntity<List<AgencyResponse>> findAllAgenciesPages(@PathVariable int page, @PathVariable int count) {
 
             List<AgencyGatewayResponse> agencies = findAllAgenciesUseCase.execute();
 
